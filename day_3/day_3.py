7e4d03064416a1c2a30046a0345482a33c51c047
Freddie <frederickbeak@gmail.com>
2024-12-04 19:05:39 +0000

    problem 2 day 3

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -8,11 +8,10 @@ import re
 current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, "input.txt")
 
-bad_data = []
+bad_data = ""
 with open(file_path) as file:
     for line in file:
-        bad_data.append(line)
-
+        bad_data += line
 # %% PROBLEM 1
 
 
@@ -32,5 +31,29 @@ print(
         for bad_data_str in bad_data
     )
 )
+
+# %% PROBLEM 2
+
+
+def find_valid_and_conditionals(bad_data: str) -> str:
+    valid_codes = re.findall(r"^(do\(\)|mul\(\d{1,3},\d{1,3}\)|don't\(\))", bad_data)
+    return valid_codes
+
+
+def conditional_calc(bad_data: str) -> int:
+    sum = 0
+    mul_switch = 1
+    ops = find_valid_and_conditionals(bad_data)
+    for op in ops:
+        if op == "do()":
+            mul_switch = 1
+        elif op == "don't()":
+            mul_switch = 0
+        else:
+            sum += mul_switch * calc(op)
+    return sum
+
+
+print(conditional_calc(bad_data))
 
 # %%
