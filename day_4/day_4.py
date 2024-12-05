cac7b10f360b766b52b8b6d2d305e7d5b4a1f232
freddie <frederick.beak@gmail.com>
2024-12-05 23:20:50 +0000

    problem 2 day 4

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,6 +1,5 @@
 # %% SETUP
 import os
-import os
 import pandas as pd
 import numpy as np
 import re
@@ -17,6 +16,7 @@ with open(file_path) as file:
 
 import numpy as np
 
+
 def get_diagonals(matrix: pd.DataFrame):
     diags = []
 
@@ -27,6 +27,7 @@ def get_diagonals(matrix: pd.DataFrame):
 
     return diags
 
+
 def get_rows_and_cols(matrix: pd.DataFrame):
     paths = []
     for i in range(matrix.shape[1]):
@@ -36,14 +37,45 @@ def get_rows_and_cols(matrix: pd.DataFrame):
 
     return paths
 
+
 def find_xmas(path: list[str]) -> int:
     return len(re.findall(r"XMAS", "".join(path))) + len(
         re.findall(r"SAMX", "".join(path))
     )
+
 
 r_diags = get_diagonals(pd.DataFrame(readings))
 l_diags = get_diagonals(np.fliplr(pd.DataFrame(readings)))
 rows_and_cols = get_rows_and_cols(pd.DataFrame(readings))
 print(sum(find_xmas(path) for path in l_diags + r_diags + rows_and_cols))
 
-# %%
+# %% PROBLEM 2
+
+def find_crosses(matrix: pd.DataFrame) -> int:
+    count = 0
+
+    def check_off_diag(i: int, j: int, tl_criteria: str, br_criteria: str) -> bool:
+        return (
+            matrix.iloc[i - 1, j - 1] == tl_criteria
+            and matrix.iloc[i + 1, j + 1] == br_criteria
+        )
+
+    def check_lead_diag(i: int, j: int, tr_criteria: str, bl_criteria: str) -> bool:
+        return (
+            matrix.iloc[i + 1, j - 1] == tr_criteria
+            and matrix.iloc[i - 1, j + 1] == bl_criteria
+        )
+
+    for i in range(1, matrix.shape[0]-1):
+        for j in range(1, matrix.shape[1]-1):
+            if matrix.iloc[i, j] == "A":
+                off = check_off_diag(i, j, "M", "S") or check_off_diag(i, j, "S", "M")
+                lead = check_lead_diag(i, j, "M", "S") or check_lead_diag(
+                    i, j, "S", "M"
+                )
+                if off and lead:
+                    count += 1
+
+    return count
+
+print(find_crosses(pd.DataFrame(readings)))
