52f9aa9b4c21738c4844dd0d44c3013296ca0294
Freddie <frederickbeak@gmail.com>
2024-12-21 00:00:21 +0000

    update

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -17,15 +17,11 @@ with open(file_path) as file:
 # %% PROBLEM 1
 
 
-def chunk_disk(disk: str, chunk_size: int) -> list[str]:
-    return [disk[i : i + chunk_size] for i in range(0, len(disk), chunk_size)]
-
-
 def uncompress(disk: str) -> list[str]:
     uncompressed_disk = []
     for i, bit in enumerate(disk):
         if i % 2 == 0:
-            uncompressed_disk += list(str(int(i / 2)) * int(bit))
+            uncompressed_disk += [int(i / 2) for _ in range(int(bit))]
         else:
             uncompressed_disk += list("." * int(bit))
     return uncompressed_disk
@@ -46,7 +42,6 @@ def check_sum(uncompressed_disk: list[str]) -> int:
     return sum(left_sums) + sum(right_sums)
 
 
-# print(check_sum("".join([uncompress(chunk) for chunk in chunk_disk(disk[0], 1000)])))
 print(check_sum(uncompress(disk[0])))
 
 # %%
