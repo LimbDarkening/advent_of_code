c3685e3dbddd0eb4e151f785351695e770fb0dc5
Freddie <frederickbeak@gmail.com>
2024-12-23 11:44:33 +0000

    broken day 9 problem 2

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 import re
 import sys
+from collections import defaultdict
 
-sys.set_int_max_str_digits(0)
 current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, "input.txt")
 
@@ -17,7 +17,7 @@ with open(file_path) as file:
 # %% PROBLEM 1
 
 
-def uncompress(disk: str) -> list[str]:
+def uncompress(disk: str) -> list[int]:
     uncompressed_disk = []
     for i, bit in enumerate(disk):
         if i % 2 == 0:
@@ -43,5 +43,61 @@ def check_sum(uncompressed_disk: list[str]) -> int:
 
 
 print(check_sum(uncompress(disk[0])))
+
+# %% PROBLEM 2
+
+
+def file_decompress(disk: str) -> list[list[str]]:
+    uncompressed_disk = []
+    for i, bit in enumerate(disk):
+        if bit == "0":
+            continue
+        if i % 2 == 0:
+            uncompressed_disk.append([int(i / 2) for _ in range(int(bit))])
+        else:
+            uncompressed_disk.append(list("." * int(bit)))
+    return uncompressed_disk
+
+
+def file_orderer(uncompressed_disk: list[list[str]]) -> list[tuple[int, int]]:
+
+    # build space dictionary
+    space = defaultdict(list)
+    for i, row in enumerate(uncompressed_disk):
+        if row[0] == ".":
+            space[len(row)].append(i)
+    max_space = max(space.keys())
+
+    file_order = []
+    for i, row in enumerate(reversed(uncompressed_disk)):
+        if row[0] != ".":
+            possible_sizes = range(len(row), max_space + 1)
+            set_ = False
+            for size in possible_sizes:
+                if len(space[size]) != 0:
+                    file_order.append((row, space[size].pop(0)))
+                    set_ = True
+                    break
+            if not set_:
+                file_order.append((row, len(uncompressed_disk) - i))
+
+    return file_order
+
+
+def file_sum(file_order: list[tuple[int, int]]) -> int:
+    s = 0
+    for file in file_order:
+        s += sum(
+            [
+                i * j
+                for i, j in zip(
+                    file[0], list(range(file[1], file[1] + len(file[0]) + 1))
+                )
+            ]
+        )
+    return s
+
+
+print(file_sum(file_orderer(file_decompress("2333133121414131402"))))
 
 # %%
